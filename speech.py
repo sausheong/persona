d7db0301d24bf020986bb6a1b2058cbfcb1895d4
Chang Sau Sheong <sausheong@gmail.com>
2023-12-19 23:48:03 +0800

    updated

--- speech.py
+++ speech.py
@@ -2,7 +2,7 @@ from config import *
 from openai import OpenAI
 
 def generate_speech(voice, text):
-	client = OpenAI(api_key="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")
+	client = OpenAI(api_key="")
 	response = client.audio.speech.create(
 		model="tts-1",
 		voice=voice,
